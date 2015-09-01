4468bf74193a6c75bd63c43c84b0a94a740b071e
michael <mike.bdg@gmail.com>
2015-09-01 18:50:28 +0800

    Implement Parser

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -11,6 +11,8 @@ public class Parser {
 		int index = 0;
 		
 		while (index < args.length) {
+			
+			// Parse the mode
 			if (args[index].equals("-m") || args[index].equals("--mode")) {
 				if (index + 1 < args.length) {
 					if (args[index + 1].toLowerCase().equals("pipe")) {
@@ -28,16 +30,10 @@ public class Parser {
 				
 				index++;
 				
+			// Parse the input txtfile name				
 			} else if (args[index].equals("-i") || args[index].equals("--input")) {
 				if (index + 1 < args.length) {
-					if (args[index + 1].toLowerCase().equals("pipe")) {
-						parserResult.setMode(Mode.PIPE_AND_FILTER);
-					} else if (args[index + 1].toLowerCase().equals("repo")) {
-						parserResult.setMode(Mode.SHARED_REPOSITORY);
-					} else {
-						parserResult.setValid(false);
-						break;
-					}
+					parserResult.setInputFile(args[index + 1]);
 				} else {
 					parserResult.setValid(false);
 					break;
@@ -45,15 +41,22 @@ public class Parser {
 				
 				index++;
 				
-			}
-			
+			// Parse the noise words txtfile name
+			} else if (args[index].equals("-n") || args[index].equals("--noise")) {
+				if (index + 1 < args.length) {
+					parserResult.setNoiseWordsFile(args[index + 1]);
+				} else {
+					parserResult.setValid(false);
+					break;
+				}
+				
+				index++;
+				
+			} 
 			
 			index++;
 		}
 				
 		return parserResult;
-	}
-	
-	
-	
+	}	
 }
